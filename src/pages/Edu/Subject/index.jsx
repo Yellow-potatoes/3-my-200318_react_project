d5ecf1316501b4f1975c09cb4d775ee9980d2b16
Zhang Tao <191094610@qq.com>
2020-08-16 14:34:30 +0800

    day10练习提交一

---
+++ src/pages/Edu/Subject/index.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import { Card, Button, Table } from 'antd';
+import { PlusOutlined } from '@ant-design/icons'
+
+
+export default class Subject extends Component {
+  render() {
+    // 表格中的数据源(此时是假数据，后期一定通过请求从服务器那边获取)
+    const dataSource = [
+      {
+        _id: '1', //数据的唯一标识
+        name: '测试学科一', //名字
+        caozuo: 'caozuo',
+      },
+      {
+        _id: '2',
+        name: '测试学科二',
+        caozuo:'caozuo',
+      },
+    ];
+    // 表格的列配置(根据设计文档写)
+    const columns = [
+      {
+        title: '分类名',
+        dataIndex: 'name',
+        key: '0',
+      },
+      {
+        title: '操作',
+        width:'200px',
+        dataIndex: 'caozuo',
+        align:'center',
+        key: '1',
+      },
+      
+    ];
+    return (
+      <div>
+        <Card title={<Button type="primary" ><PlusOutlined />新增课程分类</Button>}>
+          <Table dataSource={dataSource} columns={columns} rowKey="_id" />
+        </Card>
+      </div>
+    )
+  }
+}
